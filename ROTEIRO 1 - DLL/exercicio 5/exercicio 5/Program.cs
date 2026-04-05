1e15b00b56ef94953a9899065e3b4dc91c8e7c96
Theuz098 <matheusq402@gmail.com>
2026-04-05 20:04:34 -0300

    Add files via upload

---
+++ ROTEIRO 1 - DLL/exercicio 5/exercicio 5/Program.cs
@@ -0,0 +1,19 @@
+﻿using BibliotecaDeDesconto;
+
+
+CalculadoraDesconto calc = new CalculadoraDesconto();
+
+
+Console.WriteLine("\nCalculadora de Desconto ");
+Console.Write("Digite o valor do produto: ");
+double valor = double.Parse(Console.ReadLine());
+Console.Write("Digite o percentual de desconto: ");
+double percentual = double.Parse(Console.ReadLine());
+Console.WriteLine("Valor com desconto: R$" + calc.AplicarDesconto(valor, percentual));
+
+Console.WriteLine("\n Calculadora de Frete ");
+Console.Write("Digite o peso do produto (kg): ");
+double peso = double.Parse(Console.ReadLine());
+Console.Write("Digite a distancia em km: ");
+double distancia = double.Parse(Console.ReadLine());
+Console.WriteLine("Valor do frete: R$" + calc.CalcularFrete(peso, distancia));
