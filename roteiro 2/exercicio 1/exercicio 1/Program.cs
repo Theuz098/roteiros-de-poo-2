1a2bdf1bf4264cc695baeb67b61c30b522e13588
Theuz098 <matheusq402@gmail.com>
2026-04-05 04:50:32 -0300

    Add files via upload

---
+++ roteiro 2/exercicio 1/exercicio 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Xml.Linq;
+
+class Program
+{
+    static void Main()
+    {
+         XDocument doc = XDocument.Load(@"C:\Users\Mateus\source\repos\exercicio 1\alunos.xml");
+
+        var alunos = doc.Root.Elements("aluno");
+
+        foreach (var aluno in alunos)
+        {
+            string nome = aluno.Element("nome").Value;
+            string curso = aluno.Element("curso").Value;
+
+            Console.WriteLine($"Nome: {nome} | Curso: {curso}");
+        }
+    }
+}
