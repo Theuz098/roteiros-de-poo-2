1f81de3c06f3ea1d57caf7bd283f996d7fa940e9
Theuz098 <matheusq402@gmail.com>
2026-04-05 20:03:00 -0300

    roteiro 1- DLL

---
+++ ROTEIRO 1 - DLL/BibliotecaConversao/AppConversao/Program.cs
@@ -0,0 +1,22 @@
+﻿using BibliotecaConversao;
+
+Conversao conv = new Conversao();
+
+Console.WriteLine("Conversão de Temperatura ");
+Console.Write("Digite a temp em Celsius: ");
+double celsius = double.Parse(Console.ReadLine());
+Console.WriteLine("Resultado: " + conv.CelsiusParaFahrenheit(celsius) + "°F");
+
+Console.WriteLine("");
+Console.WriteLine("Conversão de Distância");
+Console.Write("Digite os metros: ");
+double metros = double.Parse(Console.ReadLine());
+Console.WriteLine("Resultado: " + conv.MetrosParaQuilometros(metros) + "km");
+
+Console.WriteLine("");
+Console.WriteLine("Conversão de Moeda");
+Console.Write("Digite o valor: ");
+double valor = double.Parse(Console.ReadLine());
+Console.Write("Digite a taxa de cambio: ");
+double taxa = double.Parse(Console.ReadLine());
+Console.WriteLine("Resultado: " + conv.ConverterMoeda(valor, taxa));
