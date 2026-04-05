ce2106ee475773351512bd3c6a3367d1f1b14b4c
Theuz098 <matheusq402@gmail.com>
2026-04-05 20:05:30 -0300

    Add files via upload

---
+++ ROTEIRO 1 - DLL/BibliotecaGeometria/AppGeometria/Program.cs
@@ -0,0 +1,17 @@
+﻿using BibliotecaGeometria;
+
+Geometria geo = new Geometria();
+
+Console.WriteLine(" Calculadora de Geometria ");
+
+Console.Write("Digite a largura do retangulo: ");
+double largura = double.Parse(Console.ReadLine());
+Console.Write("Digite a altura do retangulo: ");
+double altura = double.Parse(Console.ReadLine());
+Console.WriteLine("Area do Retangulo: " + geo.AreaRetangulo(largura, altura));
+Console.WriteLine("Perimetro do Retangulo: " + geo.PerimetroRetangulo(largura, altura));
+
+Console.WriteLine("");
+Console.Write("Digite o raio do circulo: ");
+double raio = double.Parse(Console.ReadLine());
+Console.WriteLine("Area do Circulo: " + geo.AreaCirculo(raio));
---
+++ ROTEIRO 1 - DLL/BibliotecaGeometria/BibliotecaGeometria/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace BibliotecaGeometria
+{
+    public class Geometria
+    {
+        public double AreaRetangulo(double largura, double altura)
+        {
+            return largura * altura;
+        }
+
+        public double AreaCirculo(double raio)
+        {
+            return Math.PI * raio * raio;
+        }
+
+        public double PerimetroRetangulo(double largura, double altura)
+        {
+            return 2 * (largura + altura);
+        }
+    }
+}
