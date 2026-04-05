5987fa8eb7558cdd8a9f6868d45086fd43d4ed50
Theuz098 <matheusq402@gmail.com>
2026-04-05 17:39:06 -0300

    Add files via upload

---
+++ roteiro 2 - XML/ExercicioXML2/ExercicioXML2/ListasProdutos.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+[XmlRoot("Produtos")]
+public class ListaProdutos
+{
+    [XmlElement("Produto")]
+    public List<Produto> Produtos { get; set; }
+}
