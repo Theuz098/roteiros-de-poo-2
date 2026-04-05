358566641d9c0c1f77fa0d8221573c1e9f49d861
Theuz098 <matheusq402@gmail.com>
2026-04-05 17:39:50 -0300

    Add files via upload

---
+++ roteiro 2 - XML/ExercicioXML4/ExercicioXML4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+class Program
+{
+    static async Task Main()
+    {
+        string url = "https://www.w3schools.com/xml/simple.xml";
+
+        
+        using (HttpClient client = new HttpClient())
+        {
+            
+            string response = await client.GetStringAsync(url);
+
+        
+            XDocument doc = XDocument.Parse(response);
+
+            Console.WriteLine("=== Cardápio ===\n");
+
+          
+            foreach (var food in doc.Root.Elements("food"))
+            {
+                string nome = food.Element("name").Value;
+                string preco = food.Element("price").Value;
+
+                Console.WriteLine($"Nome: {nome} | Preço: {preco}");
+            }
+        }
+    }
+}
