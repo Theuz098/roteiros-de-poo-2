f26a18728bcaea71a2aa2d126af21cdd049b597d
Theuz098 <matheusq402@gmail.com>
2026-04-05 20:08:27 -0300

    Add files via upload

---
+++ ROTEIRO 1 - DLL/BibliotecaValidacoes/AppValidacoes/Program.cs
@@ -0,0 +1,17 @@
+﻿using BibliotecaValidacoes;
+
+ValidadorCPF cpf = new ValidadorCPF();
+ValidadorEmail email = new ValidadorEmail();
+ValidadorSenha senha = new ValidadorSenha();
+
+Console.Write("Digite o CPF (so numeros): ");
+string cpfDigitado = Console.ReadLine();
+Console.WriteLine("CPF valido: " + cpf.Validar(cpfDigitado));
+
+Console.Write("Digite o Email: ");
+string emailDigitado = Console.ReadLine();
+Console.WriteLine("Email valido: " + email.Validar(emailDigitado));
+
+Console.Write("Digite a Senha: ");
+string senhaDigitada = Console.ReadLine();
+Console.WriteLine("Senha valida: " + senha.Validar(senhaDigitada));
