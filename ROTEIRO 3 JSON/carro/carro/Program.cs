663b0720d01c86a3d62d8ebe2380dca163393845
Theuz098 <matheusq402@gmail.com>
2026-04-05 17:30:37 -0300

    Add files via upload

---
+++ ROTEIRO 3 JSON/carro/carro/Program.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+
+class Carro
+{
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+    public int Ano { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+     
+        List<Carro> carros = new List<Carro>
+        {
+            new Carro { Marca = "Toyota", Modelo = "Corolla", Ano = 2020 },
+            new Carro { Marca = "Honda", Modelo = "Civic", Ano = 2021 },
+            new Carro { Marca = "Ford", Modelo = "Ka", Ano = 2019 }
+        };
+
+       
+        string json = JsonConvert.SerializeObject(carros, Formatting.Indented);
+        File.WriteAllText("carros.json", json);
+        Console.WriteLine("Arquivo gravado com sucesso!\n");
+
+      
+        string jsonLido = File.ReadAllText("carros.json");
+        List<Carro> carrosLidos = JsonConvert.DeserializeObject<List<Carro>>(jsonLido);
+
+        foreach (Carro c in carrosLidos)
+        {
+            Console.WriteLine($"Marca: {c.Marca} | Modelo: {c.Modelo} | Ano: {c.Ano}");
+        }
+    }
+}
---
+++ ROTEIRO 3 JSON/jobject/jobject/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using Newtonsoft.Json.Linq;
+
+class Program
+{
+    static void Main()
+    {
+    
+        string jsonConfig = @"{
+            'Servidor': 'localhost',
+            'Porta': 3306,
+            'Usuario': 'admin'
+        }";
+
+        JObject config = JObject.Parse(jsonConfig);
+
+        Console.WriteLine("Antes da alteração:");
+        Console.WriteLine($"Servidor: {config["Servidor"]}");
+        Console.WriteLine($"Porta: {config["Porta"]}");
+        Console.WriteLine($"Usuario: {config["Usuario"]}");
+
+    
+        config["Porta"] = 5432;
+
+        Console.WriteLine("\nDepois da alteração:");
+        Console.WriteLine($"Servidor: {config["Servidor"]}");
+        Console.WriteLine($"Porta: {config["Porta"]}");
+        Console.WriteLine($"Usuario: {config["Usuario"]}");
+    }
+}
