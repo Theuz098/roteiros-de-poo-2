b1ca3a1ef56a6abaf8ccac6034242c6bfc47a5b2
Theuz098 <matheusq402@gmail.com>
2026-04-05 04:51:10 -0300

    Add files via upload

---
+++ roteiro 2/exercicios3/exercicios3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Xml.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string caminho = @"C:\Users\Mateus\source\repos\exercicios3\estoque.xml";
+
+
+        XDocument doc = XDocument.Load(caminho);
+
+      
+        foreach (var item in doc.Root.Elements("item"))
+        {
+            if (item.Element("nome").Value == "Mouse")
+            {
+    
+                item.Element("quantidade").Value = "10";
+                Console.WriteLine("Mouse atualizado para quantidade 10!");
+            }
+        }
+
+
+        doc.Save(caminho);
+
+        Console.WriteLine(doc.ToString());
+    }
+}
